81789b5902ee4dd04f46da96f8d816f869c3be05
Uday Kiran <40133834+uday7981@users.noreply.github.com>
2023-07-04 00:34:14 +0530

    Add files via upload

---
+++ basics_js/hard/todolist.js
@@ -0,0 +1,13 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+
+*/
+
+

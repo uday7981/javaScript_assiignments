23a96742ea20dd9170aea305a9a21d08a585aa9b
Uday Kiran <40133834+uday7981@users.noreply.github.com>
2023-07-06 23:31:14 +0530

    Update amountAnalysis.js

--- basics_js/easy/amountAnalysis.js
+++ basics_js/easy/amountAnalysis.js
@@ -6,3 +6,65 @@
 
 
 */
+
+
+function solve(transactions) {
+  var spendEstimates = {};
+
+  for (var i = 0; i < transactions.length; i++) {
+    var t = transactions[i];
+    if (spendEstimates[t.category]) {
+      spendEstimates[t.category] = spendEstimates[t.category] + t.price;
+    } else {
+      spendEstimates[t.category] = t.price;
+    }
+  }
+  var keys = Object.keys(spendEstimates);
+  console.log(spendEstimates);
+  
+  var answer = [];
+  for (var j = 0; j < keys.length; j++) {
+    var category = keys[j];
+    var obj = {
+      category_name: category,
+      total_amount_spent_on_category: spendEstimates[category]
+    };
+    answer.push(obj);
+  }
+  console.log(answer);
+}
+
+var transactions = [
+  {
+    itemname: "pepsi",
+    category: "beverages",
+    price: 20,
+    timestamp: "2019-01-01",
+  },
+  {
+    itemname: "coke",
+    category: "beverages",
+    price: 30,
+    timestamp: "2019-01-01",
+  },
+  {
+    itemname: "fanta",
+    category: "beverages",
+    price: 40,
+    timestamp: "2019-01-01",
+  },
+  {
+    itemname: "lays",
+    category: "snacks",
+    price: 20,
+    timestamp: "2019-01-01",
+  },
+  {
+    itemname: "kurkure",
+    category: "snacks",
+    price: 20,
+    timestamp: "2019-01-01",
+  },
+];
+
+solve(transactions);

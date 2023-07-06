bdfb1bf7acc8f461bdc24406db0fefd2802855c9
Uday Kiran <40133834+uday7981@users.noreply.github.com>
2023-07-06 23:30:35 +0530

    Update anagram.js

--- basics_js/easy/anagram.js
+++ basics_js/easy/anagram.js
@@ -6,3 +6,23 @@
   
 */
 
+
+function isSorted(str) {
+  var arr = str.split('');  //['r', 'a', 'c', 'e']
+  var sorted = arr.sort(); // ['a', 'c', 'e', 'r']
+  var sortedString = sorted.join(''); // ['acer' 'race']
+  return sortedString;
+}
+
+function isAnagram(str1, str2) {
+  var str1 = str1.toUpperCase();
+  var str2 = str2.toUpperCase();
+  if(isSorted(str1) === isSorted(str2)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var ans = isAnagram('Poor', 'roop'); //true 
+console.log(ans);
